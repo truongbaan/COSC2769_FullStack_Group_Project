2ab4d275bb5c14e704f74fb2a643afe3bc9daec5
An Truong <truongbaansoftware@gmail.com>
2025-08-05 16:31:39 +0700

    Create requireAuth.ts
    a middleware that would verify valid user before able to call any api that isnt signIn-signUp

---
+++ backend/src/middleware/requireAuth.ts
@@ -0,0 +1,28 @@
+/* RMIT University Vietnam 
+# Course: COSC2769 - Full Stack Development 
+# Semester: 2025B 
+# Assessment: Assignment 02 
+# Author: Truong Ba An
+# ID: s3999568 */
+
+import { type Request, type Response, type NextFunction } from 'express'
+import { ErrorJsonResponse } from "../utils/json_mes"
+import { supabase } from "../db/db"
+
+async function requireAuth(req: Request, res: Response, next: NextFunction) {
+    const token = req.cookies.access_token
+    console.log('Access token:', token)
+    if (!token) {
+        return ErrorJsonResponse(res, 401, 'No token provided')
+    }
+    
+    // Verify token with Supabase
+    const { data, error } = await supabase.auth.getUser(token)
+    if (error || !data.user) {
+        return ErrorJsonResponse(res, 401, 'Unauthorized: Invalid token')
+    }
+    
+    // Attach user info to request if needed
+    ; (req as any).user = data.user
+    next()
+}
