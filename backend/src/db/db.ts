a1d1390b4643968155eca23fcd9e9e2c9c34ea69
An Truong <truongbaansoftware@gmail.com>
2025-08-05 16:28:24 +0700

    add supabase connect db
    db is available, could do signIn and signUp, but there is no check for create account yet, will have to recheck at service or router logic

---
+++ backend/src/db/db.ts
@@ -0,0 +1,99 @@
+/* RMIT University Vietnam 
+# Course: COSC2769 - Full Stack Development 
+# Semester: 2025B 
+# Assessment: Assignment 02 
+# Author: Truong Ba An
+# ID: s3999568 */
+
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
+
+//if needed for Json
+export type Json =
+    | string
+    | number
+    | boolean
+    | null
+    | { [key: string]: Json }
+    | Json[]
+
+// ----- Database schema -----
+// Define schema once so all CRUD calls infer Row/Insert/Update types automatically
+// so later if create new table, could use this for easier coding
+// this is not real db yet, just a sample how ut looks like
+export interface Database {
+    public: {
+        Tables: {
+            users: {
+                // .select() for table 'users'
+                Row: {
+                    id: string // using uuid to create this
+                    username: string
+                    profile_picture: string
+                    role: string
+                    email: string
+                }
+                // .insert() for table 'users'
+                Insert: {
+                    username: string
+                    profile_picture: string
+                    role: string
+                    email: string
+                }
+                // .update() in table 'users'
+                Update: {
+                    profile_picture: string
+                }
+            }
+            // add other tables here...
+        }
+        Views: {}
+        Functions: {}
+        Enums: {}
+    }
+}
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Client
+const supabaseUrl = process.env.VITE_SUPABASE_URL!
+const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY!
+
+
+if (!supabaseUrl || !supabaseKey) {
+    console.error('Supabase URL or Anon Key is missing from environment variables!')
+}
+
+export const supabase: SupabaseClient<Database> = createClient < Database > (
+    supabaseUrl,
+    supabaseKey
+)
+
+export async function signInUser(email: string, password: string):
+    Promise<null | { user: any; access_token: string; refresh_token: string }> {
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    })
+
+    if (error) {
+        console.error('Error signing in:', error.message)
+        return null
+    }
+    return data.session
+}
+
+export async function signUpUser(email: string, password: string):
+    Promise<null | { user: any; access_token: string; refresh_token: string }> {
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+    });
+
+    if (error) {
+        console.error('Error signing up:', error.message);
+        return null;
+    }
+
+    return data.session;
+}
