f8c483a52fe412c8d496a178903f153376e95b4b
An Truong <truongbaansoftware@gmail.com>
2025-08-07 16:32:15 +0700

    some sample code for other to know what to write in router and service

--- backend/src/routes/user.router.ts
+++ backend/src/routes/user.router.ts
@@ -1,8 +1,37 @@
+/* RMIT University Vietnam 
+# Course: COSC2769 - Full Stack Development 
+# Semester: 2025B 
+# Assessment: Assignment 02 
+# Author: Truong Ba An
+# ID: s3999568 */
+
 import { Router, Request, Response } from 'express';
 import { ErrorJsonResponse, SuccessJsonResponse } from '../utils/json_mes';
+import { UserService } from '../service/user.service'
 
-const userRouter = Router();
+const UserRouter = Router();
 
-userRouter.get('/users', async (req: Request, res: Response) => {
-    
+//getting all users from db
+UserRouter.get('/', async (req: Request, res: Response) => {
+    const queries = Object.keys(req.query)
+    try {
+        if (queries.length === 0) {
+            const users: any[] = await UserService.getAllUsers()
+            return SuccessJsonResponse(res, 200, users)
+        }
+        if (queries.length === 1 && 'id' in req.query) { // this is same as ?id=number
+            
+            const user = await UserService.getUserById(req.query.id)
+                
+            if (!user) { // return data is null
+                return ErrorJsonResponse(res, 404, 'user not found')
+            }
+            return res.json(user)
+        }
+        return ErrorJsonResponse(res, 500, 'Invalid query parameters')
+    } catch (error) {
+        return ErrorJsonResponse(res, 500, 'Failed to fetch user(s)')
+    }
 })
+
+export default UserRouter
--- backend/src/service/user.service.ts
+++ backend/src/service/user.service.ts
@@ -1,1 +1,46 @@
-//nothing here yet
+import { supabase } from "../db/db"
+
+export const UserService = {
+    /** Fetch all users*/
+    async getAllUsers(): Promise<any[]> {
+        const { data, error } = await supabase
+            .from('users')
+            .select('*')
+            .order('id', { ascending: false })
+
+        //DEBUG, will be remove
+        console.log('📊 Raw Supabase response:')
+        console.log('  - Data:', data)
+        console.log('  - Error:', error)
+        console.log('  - Data length:', data?.length)
+        //
+
+        if (error) {
+            console.error('Error fetching user:', error)
+            throw error
+        }
+        console.log(data)
+        return data!
+    },
+
+    /** Fetch a single user by id */
+    async getUserById(id: string | any): Promise<any> {
+        const { data, error } = await supabase
+            .from('users')
+            .select('*')
+            .eq('id', id)
+            .maybeSingle()
+
+        console.log(data)
+
+        if (error) {
+            console.error(`Error fetching user ${id}:`, error)
+            throw error
+        }
+        if (!data) {
+            return null  // explicitly return null to trigger 404 in route
+        }
+
+        return data
+    },
+}
