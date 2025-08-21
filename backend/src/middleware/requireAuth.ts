1dc23d0026624dc59ff85b4a2b84c79d0bdca670
An Truong <truongbaansoftware@gmail.com>
2025-08-21 15:54:50 +0700

    Update requireAuth.ts
    return id and role of the user

--- backend/src/middleware/requireAuth.ts
+++ backend/src/middleware/requireAuth.ts
@@ -14,6 +14,7 @@ declare global {
     namespace Express {
         interface Request {
             user_id: string;
+            user_role?: string; // for verifying role of router with multiple role allowed
         }
     }
 }
@@ -24,28 +25,32 @@ export function requireAuth(role: string | string[] = '') {
         if (!token) {
             return ErrorJsonResponse(res, 401, 'No token provided')
         }
-        
+
         // Verify token with Supabase
         const { data, error } = await supabase.auth.getUser(token)
         if (error || !data.user) {
             return ErrorJsonResponse(res, 401, 'Unauthorized: Invalid token')
         }
-        
+
         const roles: string[] =
             typeof role === 'string' ? role
-                    ? [role] // non-empty string → wrap in array
-                    : []     // empty string → no restriction
+                ? [role] // non-empty string → wrap in array
+                : []     // empty string → no restriction
                 : role;
-        
+
+        //get user info
+        const user = await UserService.getUserById(data.user.id, false);
+        if (!user) {
+            return ErrorJsonResponse(res, 404, 'Unknown user in database but valid in auth! PLEASE delete that user from auth!');
+        }
+
         if (roles.length > 0) {
-            const user = await UserService.getUserById(data.user.id, false);
-            if (!user) {
-                return ErrorJsonResponse(res,404,'Unknown user in database but valid in auth! PLEASE delete that user from auth!');
-            }
             if (!roles.includes(user.role)) {
-                return ErrorJsonResponse(res,403,`Unauthorized: only roles [${roles.join(', ')}] can access this resource`);
+                return ErrorJsonResponse(res, 403, `Unauthorized: only roles [${roles.join(', ')}] can access this resource`);
             }
         }
+
+        req.user_role = data.user.role
         req.user_id = data.user.id//return user_id field for other controller uses
         next();
     };
