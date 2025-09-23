a8acab26d381d6ce315b17a643821be2229eebc7
An Truong <truongbaansoftware@gmail.com>
2025-09-23 16:56:32 +0700

    adjust for server-side limitation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,15 +10,25 @@ import express from 'express';
 import apiRouter from './routes/router';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express()
+
+const generalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 450,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: "Too many requests from this IP, please try again after 15 minutes."
+});
+
 
 //for cookies :D
 app.use(cookieParser());
 
 app.use(
   cors({
-    origin: true, // Allow all origins
+    origin: ['http://localhost:3000', 'https://cosc-2769-full-stack-group-project.vercel.app'],
     credentials: true,
   })
 );
@@ -28,7 +38,7 @@ app.use(
 app.use(express.json());
 
 // Mount the main API router under the '/api' base path.
-app.use("/api", apiRouter);
+app.use("/api", generalLimiter , apiRouter);
 
 // Health check endpoint
 app.get("/health", (req, res) => {
